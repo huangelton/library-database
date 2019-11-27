3565695b203544308bb412deb01d46e0074b9a1b
elton_h <47727733+huangelton@users.noreply.github.com>
2019-11-26 19:57:29 -0800

    Update system.h

--- system.h
+++ system.h
@@ -1,6 +1,7 @@
 class Library {
 
     private:
+    
         struct Node {
             std::string bookTitle; // book title
             std::string authorName; // author name
@@ -11,14 +12,34 @@ class Library {
         Node *head; // head pointer
    
     public:
+    
         Library(); // default constructor
         ~Library(); // destructor
         Library(const Library &objList); // copy constructor
         Library & operator=(const Library &objList); // copy assignment
-        void addData(std::string, std::string, long, double); // insert node   
+        void addData(std::string, std::string, long, double); // insert node
+        // precondition: valid string, long int, and double 
+        // postcondition: none
+    
         bool searchData(long); // search node
+        // precondition: valid long int
+        // postcondition: return true or false
+        
         void deleteData(long); // remove node
+        // precondition: valid long int
+        // postcondition: none
+    
         void deleteItem(); // helper function
+        // precondition: none
+        // postcondition: none
+    
         int getTotalBooks(); // find total books
+        // precondition: none
+        // postcondition: return integer
+    
         void displayList() const; // display list
+        // precondition: none
+        // postcondition: none
 };
+
+
